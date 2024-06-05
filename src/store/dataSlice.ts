e968278d1c4d2e6717641c682f2d6a9b8b8c60e8
karnaukhoff <n4echduf@gmail.com>
2024-06-05 21:34:34 +0300

    dataSlice типизирован

--- src/store/dataSlice.ts
+++ src/store/dataSlice.ts
@@ -1,10 +1,19 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+// Define the interface for individual items
+interface Item {
+  id: number;
+  title: string;
+  description: string;
+  images: string[];
+}
+
+// Define the shape of the Redux state
 interface DataState {
-  all: any[];
-  filtered: any[];
-  filter:  string;
-  deleted: string;
+  all: Item[];
+  filtered: Item[];
+  filter: 'all' | 'liked';
+  deleted: '' | 'active';
 }
 
 const initialState: DataState = {
@@ -18,17 +27,17 @@ const dataSlice = createSlice({
   name: 'data',
   initialState,
   reducers: {
-    setAllData: (state, action: PayloadAction<any[]>) => {
+    setAllData: (state, action: PayloadAction<Item[]>) => {
       state.all = action.payload;
     },
-    setFilteredData: (state, action: PayloadAction<any[]>) => {
+    setFilteredData: (state, action: PayloadAction<Item[]>) => {
       state.filtered = action.payload;
     },
     setFilter(state, action: PayloadAction<'all' | 'liked'>) {
-        state.filter = action.payload;
-      },
+      state.filter = action.payload;
+    },
     setDeletedMode(state, action: PayloadAction<'' | 'active'>) {
-        state.deleted = action.payload;
+      state.deleted = action.payload;
     },
   },
 });
